b893110835d279f801122f0be683a65bc648cd42
sshimaoka <79296271+sshimaoka@users.noreply.github.com>
2021-10-18 08:14:04 +0900

    Update main.js

--- parts/main.js
+++ parts/main.js
@@ -76,6 +76,7 @@ function selectbox() {
     { value: 3, name: "イルカ" },
     { value: 4, name: "ライオン" },
   ];
+  console.log(animals);
   var i = 0;
   while(i <= 1){
     const jsSelectBox = document.querySelector(".js-selectbox");
@@ -99,59 +100,132 @@ function selectbox() {
 
 // auto match.html
 function match() {
+  //全選択肢となる部分　もしかしたらDBなどにいれこむかも
   var list1 = [
     ['for', 'while', 'do'],
     ['if', 'else', 'which'],
     ['print', 'alert']
   ];
-  var lista;
-  var sbox = {};
+  //ワードがマッチした場所を保存する
+  var lista = []; 
+  //マッチしたワードを含む選択肢を保存する
+  var sbox = [];
+  //マッチしたワードを保存する
+  var mbox = [];
+  //何回ワードがマッチしたか保存する
   var count = 0;
 
-  var btext = document.getElementById("auto").innerText;
+//
+  var btext = document.getElementById("question_area").innerText;
+
+//文字列を配列に変更
   ctext = btext.split(/(\s|\(|\)|\;|\n)/);
 //  console.log(ctext);
 
-//str.split(',').forEach( function( value ) {
+//問題とのマッチングループ
 
   for(let i = 0; i < list1.length; i++){
     for(let j = 0; j < list1[i].length; j++){
       for(let k = 0; k < ctext.length; k++){
         if(ctext[k] == list1[i][j]){
           sbox[count] = list1[i];
-          lista = list1[i];
-          console.log(lista);
-          console.log(sbox);
+          mbox[count] = ctext[k];
+          lista[count] = k;
           count += 1;
+
+          
+          console.log("mbox= " + mbox)
+          console.log("k= " + k);
+          console.log("lista=" + lista);
+          console.log("sbox=")
+          console.log(sbox);
         }
       }
     }
   }
-
-//  ctext = (String(ctext)).replace(/,/g, '');
-//  console.log(ctext);
-//  document.getElementById("auto").innerText = x;
-
-
-  
+  //lista.sort();
+  //マッチしたワード部分を選択肢に変更
+  for(let l = 0; l <lista.length; l++){
+    ctext[lista[l]] = `<select class="sele${l}"></select>`;
+  }
 
 
+// 配列から文字列に変更して表示
+  ctext = (String(ctext)).replace(/\n/g, '<br>');
+  ctext = (String(ctext)).replace(/,/g, '');
+  //console.log(ctext)
+  document.getElementById("question_area").innerHTML = ctext;
 
-  const jsSelectBox = document.querySelector(".js-selectbox");
-  const selectWrap = document.createElement("div");
-  selectWrap.classList.add("selectwrap");
-  const select = document.createElement("select");
-  select.classList.add("select");
-//  lista.forEach((v) => {
-  sbox[0].forEach((v) => {
-    const option = document.createElement("option");
-    option.textContent = v;
-    select.appendChild(option);
-  });
-  selectWrap.appendChild(select);
-  jsSelectBox.appendChild(selectWrap);
-  
+
+// ワードがマッチした場所に選択肢を生成する
+    for(let m = 0; m < count; m++){
+      var select = document.querySelector(`select.sele${m}`);
+    for(let n=0; n < sbox[m].length; n++){
+      var option = document.createElement('option');
+      option.innerText = sbox[m][n];
+      
+      //マッチしたワードと等しい箇所のValueをans その他を数字に変更
+      if(sbox[m][n] != mbox[m])
+        option.value = n;
+      else 
+        option.value = "ans";
+
+      select.append(option);
+    }
+  }
 }
+
+function hantei_auto(){
+  //正解数をカウントする
+  var count = "0";
+  //ループ回数をカウント
+  var loop ="0";
+  //セレクトボックスがいくつあるのかをカウント
+  //セレクトボックスがなかったらBreak
+  while(1){
+    var select = document.querySelector(`select.sele${loop}`);
+    if(select != null){
+      //セレクトボックスのValueを入手（正解ならans, それ以外だと数字が返る）
+      if(select.value == "ans"){
+        //何問正解しているかを合計
+        count++;
+      }
+    }else{
+      break;
+    }
+    loop++;
+  }
+
+  //判定結果を表示
+  document.getElementById("kekka").textContent = count + "問正解";
+}
+
+function kaeru(){
+  var count = "0";
+  var code = document.getElementById("question_area").innerHTML;
+  code2 = code.split(/<select|<\/select>/);
+  console.log(code2)
+  
+  for (let i = 0; i < code2.length; i++) {
+    text = "sele" + count;
+    console.log(text)
+    if(code2[i].indexOf(text) == -1){
+      console.log(i)
+    }else{
+      var obj = document.querySelector(`select.sele${count}`);
+      var idx = obj.selectedIndex;
+      var txt = obj.options[idx].text;
+      console.log(txt);
+      code2[i] = txt;
+      count++;
+    }
+  }
+  code2 = (String(code2)).replace(/,/g, '');
+//  code2 = (String(code2)).replace(/\<br\>/g, '');
+  console.log(code2)
+  document.getElementById("kaeru").innerHTML = code2;
+}
+
 
 
 
